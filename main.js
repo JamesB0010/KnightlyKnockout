d3209db9d51667295bdf216b62e1edfeb06e6796
James Bland <jammydodger8888@gmail.com>
2023-10-08 19:59:33 +0100

    Added rotating cube with standard material, made plane underneith, added orbit controls

--- main.js
+++ main.js
@@ -1,4 +1,6 @@
 import * as THREE from 'three';
+import {OrbitControls} from './node_modules/three/examples/jsm/controls/OrbitControls';
+
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
@@ -6,11 +8,25 @@ const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.inner
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize( window.innerWidth, window.innerHeight );
 document.body.appendChild( renderer.domElement );
+const orbitControls = new OrbitControls(camera, renderer.domElement);
 
 const geometry = new THREE.BoxGeometry( 1, 1, 1 );
-const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
+const material = new THREE.MeshStandardMaterial( { color: 0x00ff00 } );
 const cube = new THREE.Mesh( geometry, material );
+
+const direcLight = new THREE.DirectionalLight(0xffffff, 1);
+direcLight.position.z = 2;
+direcLight.position.y = 3;
+
+const ground = new THREE.Mesh( 
+	new THREE.BoxGeometry( 5, 0.5, 10 ), 
+	new THREE.MeshStandardMaterial( { color: 0x0000ff } ) 
+	);
+ground.position.y = -2;
+
 scene.add( cube );
+scene.add(direcLight);
+scene.add(ground);
 
 camera.position.z = 5;
 
