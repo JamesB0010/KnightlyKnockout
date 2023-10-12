630abd70f43597f8a2599f86892051e949e5fd9f
JamesB0010 <98528984+JamesB0010@users.noreply.github.com>
2023-10-12 14:05:17 +0000

    Made background blue and added antiailas to renderer

--- main.js
+++ main.js
@@ -7,7 +7,10 @@ const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 camera.position.z = 5;
 
-const renderer = new THREE.WebGLRenderer();
+const renderer = new THREE.WebGLRenderer({
+  alpha:true,
+  antialias:true
+});
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 const orbitControls = new OrbitControls(camera, renderer.domElement);
