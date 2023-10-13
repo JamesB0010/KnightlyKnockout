aea24fcbfedcc723883965fb92a971164ea3ddda
James Bland <jammydodger8888@gmail.com>
2023-10-13 12:01:03 +0100

    shadows working

--- main.js
+++ main.js
@@ -15,14 +15,6 @@ renderer.shadowMap.enabled = true
 // CONTROLS
 const controls = new OrbitControls(camera, renderer.domElement);
 
-// RESIZE HAMDLER
-export function onWindowResize() {
-    camera.aspect = window.innerWidth / window.innerHeight;
-    camera.updateProjectionMatrix();
-    renderer.setSize(window.innerWidth, window.innerHeight);
-}
-window.addEventListener('resize', onWindowResize);
-
 // INIT CAMERA
 camera.position.z = 25;
 camera.position.x = 3;
@@ -48,21 +40,6 @@ cone.receiveShadow = true
 cone.castShadow = true
 scene.add(cone);
 
-// CYLINDER
-const cylinder = new THREE.Mesh(new THREE.CylinderGeometry(1, 1, 6, 64), new THREE.MeshPhongMaterial({ color: 0x3ea34c }))
-cylinder.position.set(3, 3, 2.7)
-cylinder.receiveShadow = true
-cylinder.castShadow = true
-scene.add(cylinder)
-
-// TORUS
-const torus = new THREE.Mesh(new THREE.TorusGeometry(2, 0.5, 64, 64), new THREE.MeshPhongMaterial({ color: 0x2a7ab0 }))
-torus.position.set(-0.5, 2.5, 2.7)
-torus.rotateY(2.5)
-torus.receiveShadow = true
-torus.castShadow = true
-scene.add(torus)
-
 // DIRECTIONAL LIGHT
 const directionalLight = new THREE.DirectionalLight(0xffffff, 1);
 directionalLight.position.x += 20
@@ -78,49 +55,15 @@ directionalLight.shadow.camera.top = d;
 directionalLight.shadow.camera.bottom = - d;
 scene.add(directionalLight);
 
-scene.add( new THREE.CameraHelper( directionalLight.shadow.camera ) );
-
-// SPOT LIGHT
-// const spotLight = new THREE.SpotLight( 0xffffff );
-// spotLight.position.set( 20, 15, 20 );
-// spotLight.castShadow = true;
-// spotLight.shadow.mapSize.width = 4096;
-// spotLight.shadow.mapSize.height = 4096;
-// scene.add(spotLight)
-
-// POINT LIGHT
-// const light1 = new THREE.PointLight( 0xff0000, 1, 100 );
-// light1.position.set( 20, 20, 20 );
-// light1.castShadow = true;
-// light1.shadow.mapSize.width = 4096;
-// light1.shadow.mapSize.height = 4096;
-// scene.add( light1 );
-
-// const light2 = new THREE.PointLight( 0x00ff00, 1, 100 );
-// light2.position.set( 20, 20, 20 );
-// light2.castShadow = true;
-// light2.shadow.mapSize.width = 4096;
-// light2.shadow.mapSize.height = 4096;
-// scene.add( light2 );
+//scene.add( new THREE.CameraHelper( directionalLight.shadow.camera ) );
 
 
 // ANIMATE
 function animate() {
-    // TARGET
-    const time = Date.now() * 0.0005;
-    directionalLight.position.x = Math.sin(time * 0.7) * 20;
-    directionalLight.position.z = Math.cos(time * 0.7) * 20;
-
-    // spotLight.position.x = Math.sin(time * 0.7) * 20;
-    // spotLight.position.z = Math.cos(time * 0.7) * 20;
-
-    // light1.position.x = Math.sin(time) * 20;
-    // light1.position.z = Math.sin(time) * 20;
-    // light2.position.x = Math.cos(time) * -20;
-    // light2.position.z = Math.cos(time) * 20;
-
     renderer.render(scene, camera);
     requestAnimationFrame(animate);
 }
+
+
 document.body.appendChild(renderer.domElement);
 animate();
