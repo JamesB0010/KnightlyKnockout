53b59e7e9d008a9f04c34fe9b7c829776ac343e7
James Bland <jammydodger8888@gmail.com>
2023-10-13 13:36:16 +0100

    Revert "revert this"
    This reverts commit 63322fe1e28c5a367edfd6395fcc80a4f64253db.

--- main.js
+++ main.js
@@ -1,14 +1,43 @@
 import * as THREE from 'three';
 import { OrbitControls } from './node_modules/three/examples/jsm/controls/OrbitControls';
-import {GameObject} from '/GameObject.js';
-import {Game} from './Game.js';
+import {GameObject} from './GameObject.js';
 
-let game = new Game();
-game.Init();
+new GameObject();
 
-function Animate(){
-	requestAnimationFrame(Animate);
-	game.Render();
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+
+const renderer = new THREE.WebGLRenderer({
+	antialias: true
+});
+renderer.shadowMap.enabled = true;
+renderer.setSize( window.innerWidth, window.innerHeight );
+document.body.appendChild( renderer.domElement );
+
+const controls =  new OrbitControls(camera, renderer.domElement);
+
+const ambLight = new THREE.AmbientLight(0xffffff, 0.1);
+
+const cube = new GameObject();
+
+const floorGeometry = new THREE.BoxGeometry(10,0.5,10);
+const floor = new THREE.Mesh(floorGeometry, new THREE.MeshStandardMaterial({color:0xffffff}));
+floor.position.y = -2;
+floor.receiveShadow = true;
+
+const direcLight = new THREE.DirectionalLight(0xffffff, 1);
+direcLight.position.y = 2;
+direcLight.castShadow = true;
+
+
+
+scene.add( cube, floor, ambLight, direcLight, new THREE.DirectionalLightHelper(direcLight));
+
+camera.position.z = 5;
+
+function animate() {
+	requestAnimationFrame( animate );
+	renderer.render( scene, camera );
 }
 
-Animate();
+animate();
