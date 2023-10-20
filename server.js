e178d2aac5d1c16c323d8ac96ed94cb6d09720ae
JamesB0010 <98528984+JamesB0010@users.noreply.github.com>
2023-10-20 16:05:27 +0000

    trying to get express to serve

---
+++ server.js
@@ -0,0 +1,16 @@
+//credit for creating express server https://replit.com/talk/learn/SocketIO-Tutorial-What-its-for-and-how-to-use/143781
+
+
+const express = require("express"); // use express
+const app = express(); // create instance of express
+const server = require("http").Server(app); // create server
+const io = require("socket.io")(server); // create instance of socketio
+
+
+app.use(express.static("public")); // use "public" directory for static files
+app.use(express.static(""))
+
+
+io.on("connection", socket => {
+});
+server.listen(3000); // run server
