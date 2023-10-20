44da0cfbb7a069e7c1dfbb686c6bfa5cb72c198e
JamesB0010 <98528984+JamesB0010@users.noreply.github.com>
2023-10-20 13:10:05 +0000

    Adding express to server

--- Server/server.js
+++ Server/server.js
@@ -1,24 +1,32 @@
-//server init code from learn socket io in 30 minuets video https://www.youtube.com/watch?v=ZKEqqIO7n-k
-const io = require('socket.io')(3000, {
-    cors: {
-        origin: ['http://localhost:5173']
-    }
-})
+//express server setup from https://replit.com/talk/learn/SocketIO-Tutorial-What-its-for-and-how-to-use/143781
+const express = require('express');
+const app = express();
+const server = require('http').Server(app);
+const io = require('socket.io')(server);
+
+app.use(express.static('../LocalOnlyClient'));
+
+// //server init code from learn socket io in 30 minuets video https://www.youtube.com/watch?v=ZKEqqIO7n-k
+// const io = require('socket.io')(3000, {
+//     cors: {
+//         origin: ['http://localhost:5173']
+//     }
+// })
 
 
-io.on("connection", socket =>{
-    console.log("Someone joined with id" + socket.id);
+// io.on("connection", socket =>{
+//     console.log("Someone joined with id" + socket.id);
 
-    socket.emit('setId', socket.id);
+//     socket.emit('setId', socket.id);
 
-    socket.on("playerUpdatePosition", data =>{
-        socket.broadcast.emit("updateNetworkedPlayerPosition", {pos: data.pos,id: data.id})
-    })
+//     socket.on("playerUpdatePosition", data =>{
+//         socket.broadcast.emit("updateNetworkedPlayerPosition", {pos: data.pos,id: data.id})
+//     })
 
-    socket.on('disconnect', ()=>{
-        console.log(socket.id + " Disconnected");
-        socket.broadcast.emit("RemovePlayer", socket.id)
-    })
-})
+//     socket.on('disconnect', ()=>{
+//         console.log(socket.id + " Disconnected");
+//         socket.broadcast.emit("RemovePlayer", socket.id)
+//     })
+// })
 
-console.log("server running");
+// console.log("server running");
