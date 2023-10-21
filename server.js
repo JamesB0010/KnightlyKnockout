6c494531fcb59e8e0d676505d1889c5c5fedb79c
James Bland <jammydodger8888@gmail.com>
2023-10-21 15:24:42 +0100

    Added index.html, log in, sign up and 404 page, pushing to check it works on replit

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 //credit for creating express server https://replit.com/talk/learn/SocketIO-Tutorial-What-its-for-and-how-to-use/143781
+//credit to https://www.youtube.com/watch?v=OKGMhFgR7RY for responding to 404 status
+//credit to https://www.youtube.com/watch?v=JbwlM1Gu5aE for being able to send a html file as a response
 
-
+const path = require("path");
 const express = require("express"); // use express
 const app = express(); // create instance of express
 const server = require("http").Server(app); // create server
@@ -13,6 +15,12 @@ const io = require("socket.io")(server, {
 
 
 app.use(express.static("public")); // use "public" directory for static files
+app.use(express.static("views"));
+
+//configure 404 page
+app.use(function(req, res, next){
+  res.status(404).sendFile(path.join(__dirname, "views", "404.html"))
+});
 
 
 io.on("connection", socket => {
@@ -29,8 +37,6 @@ io.on("connection", socket => {
     socket.broadcast.emit("RemovePlayer", socket.id)
   })
 });
-
-
 
 server.listen(3000); // run server
 console.log("server running");
