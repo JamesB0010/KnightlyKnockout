6ff4e7147059d66fc4b8a0007d451ecb8a09b1af
James Bland <jammydodger8888@gmail.com>
2023-10-27 20:25:54 +0100

    Revert "Made GameObject class by default make a cube"
    This reverts commit 2e3fdefc91b113135736892f6209d231713dff8c.

--- public/GameObject.js
+++ public/GameObject.js
@@ -2,52 +2,48 @@ import * as THREE from 'three';
 
 const gravity = -0.002;
 
-class GameObject extends THREE.Mesh {
-  constructor({
-    height = 1,
-    width = 1,
-    depth = 1,
-    position = {
-      x: 0,
-      y: 0,
-      z: 0
-    },
-    color = 0xffffff,
-    inputEnabled = false,
-    socketId = -1,
-    geometry = new THREE.BoxGeometry(1, 1, 1),
-    material = new THREE.MeshStandardMaterial({ color })
-  }) {
-    geometry.scale(width, height, depth);
-    material.color.setHex = color;
-    super(
-      geometry,
-      material
-    );
-    this.castShadow = true;
-    this.position.x = position.x;
-    this.position.y = position.y;
-    this.position.z = position.z;
-    this.castShadow = true;
-    this.receiveShadow = true;
-    this.height = height;
-    this.width = width;
-    this.depth = depth;
+class GameObject extends THREE.Mesh{
+    constructor({
+        height = 1,
+        width = 1,
+        depth = 1,
+        position = {
+            x: 0,
+            y: 0,
+            z: 0
+        },
+        color = 0xffffff,
+        inputEnabled = false,
+        socketId = -1,
+    }){
+        super(
+            new THREE.BoxGeometry( width, height, depth), 
+            new THREE.MeshStandardMaterial( { color} )
+            );
+            this.castShadow = true;
+            this.position.x = position.x;
+            this.position.y = position.y;
+            this.position.z = position.z;
+            this.castShadow = true;
+            this.receiveShadow = true;
+            this.height = height;
+            this.width = width;
+            this.depth = depth;
 
-    this.velocity = {
-      x: 0,
-      y: 0,
-      z: 0
-    };
-    this.grounded = false;
+            this.velocity = {
+                x: 0,
+                y: 0,
+                z: 0
+            };
+            this.grounded = false;
 
-    this.bounciness = 0.6;
+            this.bounciness = 0.6;
 
-    this.socketId = socketId;
-  }
+            this.socketId = socketId;
+    }
 
-  Update() {
-  }
+    Update(){
+    }
 }
 
-export { GameObject };
+export {GameObject};
