0c3a0f30e8e958fc3618761110671539d86ecbb1
James Bland <jammydodger8888@gmail.com>
2023-11-07 20:45:07 +0000

    Now alerts when a player has won

--- public/Javascript/round-manager.js
+++ public/Javascript/round-manager.js
@@ -8,17 +8,29 @@ class RoundManager{
         this.#roundNumber = 0;
         this.#maxScore = 3;
         this.#playerScores = new Map();
+        this.enemyScoreP = document.getElementById("enemyScoreText");
     }
 
     addKeyValToMap(key, val){
         this.#playerScores.set(key,val);
     }
 
+    #CheckPlayerHasWon(){
+        this.#playerScores.forEach((value, key) =>{
+            if (value >= this.#maxScore){
+                alert("Player won");
+            }
+        })
+    }
+
     playerDead(Id){
         for (let [key, value] of this.#playerScores){
             if(key != Id){
-                this.#playerScores.set(key, this.#playerScores.get(key) + 1);
+                let newScore = this.#playerScores.get(key) + 1;
+                this.#playerScores.set(key, newScore);
+                this.enemyScoreP.innerText = newScore;
                 alert("Player dead" + Id);
+                this.#CheckPlayerHasWon();
                 return;
             }
         }
